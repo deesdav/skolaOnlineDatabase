485c5a455ddaf5b7498e745783ee686ac79bc195
deesdav <dejvisvan@seznam.cz>
2024-01-26 12:50:24 +0100

    UPDATE

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ cnx = mysql.connector.connect(
     database='skolaonline'
 )
 
-if mysql.connector:
+if cnx.is_connected():
     print("Database connected")
 
 # Create a cursor object to interact with the database
@@ -16,13 +16,16 @@ cursor = cnx.cursor()
 
 try:
     # Execute a query to insert a class record
-    cursor.execute("INSERT INTO class (ID, class_name) VALUES ('NULL', '3.Ai')")
+    cursor.execute("INSERT INTO class (class_name) VALUES ('3.Ai')")
 
     # Commit the changes to the database
     cnx.commit()
 
-    # Inserting a new student record that references the class
-    cursor.execute("INSERT INTO student (ID, first_name, last_name, birth_date, class_id) VALUES ('NULL', 'Pavel', 'Pavlos', '2000-01-01', '1')")
+    # Define the ALTER TABLE statement to add the foreign key constraint
+    alter_table_query = "ALTER TABLE student ADD CONSTRAINT student_ibfk_1 FOREIGN KEY (class_id) REFERENCES class(ID)"
+
+    # Execute the ALTER TABLE statement
+    cursor.execute(alter_table_query)
 
     # Commit the changes to the database
     cnx.commit()
@@ -36,6 +39,12 @@ try:
     # Process the result
     for row in result:
         print(row)
+
+    # Inserting a new student record that references the class
+    cursor.execute("INSERT INTO student (first_name, last_name, birth_date, class_id) VALUES ('Pavel', 'Pavlos', '2000-01-01', '1')")
+
+    # Commit the changes to the database
+    cnx.commit()
 
     # Execute a query to fetch all student data
     cursor.execute("SELECT * FROM student")
